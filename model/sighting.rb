0122a23ceec391cb1dd00cf120ea6c8802aba1d9
Nigel Garside <nigel.garside@gmail.com>
2010-07-13 19:08:59 +0100

    Change user_id to string (e.g. 'NG' or 'EW'). Simplify relationship mapping. Add two records to database, one for each user.

--- model/sighting.rb
+++ model/sighting.rb
@@ -2,8 +2,8 @@ class Sighting < Sequel::Model
 
     set_schema do
 	foreign_key :bto_code, :species,
-	    :type=>String, :size=>8, :null=>false
-	foreign_key :user_id, :user, :null=>false
+	    :type=>String, :size=>2, :null=>false
+	foreign_key :user_id, :users, :type=>String, :size=>2, :null=>false
 	Date :first_date
 	String :note
 	primary_key [:user_id, :bto_code]
@@ -11,12 +11,11 @@ class Sighting < Sequel::Model
 
     create_table unless table_exists?
     unrestrict_primary_key
-    many_to_one :species, :class=>'Species', :key=>'bto_code'
-    many_to_one :user, :class=>'User'
+    many_to_one :species, :key=>'bto_code'
+    many_to_one :user
 
     if empty?
-	create :user_id => 1,
-	    :bto_code => 'PE',
-	    :first_date => '2004-01-01'
+	create :user_id=>'NG', :bto_code=>'PE', :first_date=>'2004-01-01'
+	create :user_id=>'EW', :bto_code=>'DR', :first_date=>'2004-01-01'
     end
 end
