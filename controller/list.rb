8a6a6ad907fce4e8a02fb735584fb3729134119e
Nigel Garside <nigel.garside@gmail.com>
2010-07-13 19:13:13 +0100

    Retrieve sightings from database to display on page.

--- controller/list.rb
+++ controller/list.rb
@@ -1,5 +1,4 @@
 class ListController < Controller
-#    map '/list'
 
     def initialize
 	@isodd = false
@@ -10,16 +9,59 @@ class ListController < Controller
 	@user = login_or_user(login)
 	@title = "Peterstone List"
 
-	@species_list = {
-	    :B => 'Blackbird',
-	    :C => 'Crow',
-	    :FF => 'Fieldfare',
-	    :DR => 'Spotted Redshank',
-	}
+	@sightings = Species.eager_graph(:sightings).all
+
+#	if title
+#	    id = title[/^(\d+)-/, 1]
+#	    @blog = Blog[id]
+#	end
+
     end
+
+    def edit(id)
+	login_first
+
+#	@user, @profile = user, user.profile
+	@user = user
+#	redirect_referrer unless @post = Blog[id]
+	@legend = "Edit Entry"
+	@submit = "Update Entry"
+
+	save
+    end
+
+    def new
+	login_first
+
+#	@user, @profile = user, user.profile
+	@user = user
+#	@post = Blog.new(:profile => @profile)
+	@legend = "New Entry"
+	@submit = "Create Entry"
+
+	save
+    end
+
+    #template :new, :edit
 
     def cycle
 	(@isodd = !@isodd) ? 'even' : 'odd'
+    end
+
+   private
+
+    def save
+	return unless request.post?
+
+	@post.title = request[:title]
+	@post.body = request[:body]
+
+	if @post.save
+	    flash[:good] = "Created post"
+	    redirect self.rs(:/, @post.to_url)
+	else
+	    flash[:bad] = "Couldn't create post"
+	end
     end
 
 end
