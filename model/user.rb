a47c87bb054edbab86f6ca7388c468db6b5285ee
Nigel Garside <nigel.garside@gmail.com>
2010-07-18 13:51:14 +0100

    Add user name to User model and database. Rename argument to login.

--- model/user.rb
+++ model/user.rb
@@ -5,6 +5,7 @@ class User < Sequel::Model
 	primary_key :user_id, :char, :size=>2,
 	    :null=>false, :auto_increment=>false
 	varchar :login, :unique=>true
+	varchar :name, :size=>40
 	varchar :openid, :unique=>true
 	varchar :crypted_password, :size=>40
 	varchar :salt, :size=>40
@@ -23,11 +24,11 @@ class User < Sequel::Model
 	Digest::SHA1.hexdigest("--#{salt}--#{password}--")
     end
 
-    def self.authenticate(hash)
+    def self.authenticate(creds)
 	#	if oid = hash['openid']
 	#	    User[:openid => oid]
 	#	else
-	login, pass = hash['login'], hash['password']
+	login, pass = creds['login'], creds['password']
 
 	if user = User[:login => login]
 	    # we don't store the password in the session...
@@ -46,8 +47,9 @@ class User < Sequel::Model
     end
 
     if empty?
-	create :user_id => 'NG', :login => 'garsiden', :is_admin=>true
-	create :user_id => 'EW', :login => 'wange'
+	create :user_id => 'NG', :login => 'garsiden', :name => 'Nigel Garside',
+	    :is_admin=>true
+	create :user_id => 'EW', :login => 'wange', :name => 'Eddie Wang'
     end
 
     def authenticated?(password)
