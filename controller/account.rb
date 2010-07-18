d98e7a9b42de3201670de4276a1e8f1af80b126a
Nigel Garside <nigel.garside@gmail.com>
2010-07-18 17:24:39 +0100

    Add @title variable. Default to home page on login if no other page specified.

--- controller/account.rb
+++ controller/account.rb
@@ -1,46 +1,34 @@
-class AccountController < Controller
-#  helper :simple_captcha, :identity
-#  layout 'minimal_layout'
+class AccountController < Controller 
     layout 'minimal_layout'
 
-  # TODO: use stack
-  def login
-    redirect_referrer if logged_in?
-    push request.referrer unless inside_stack?
-
-    case request[:fail]
-    when 'session'
-      flash[:bad] =
-        'Failed to login, please make sure you have cookies enabled for this site'
+    def initialize
+	@title = "Peterstone Birds"
     end
 
-    return unless request.post?
+    def login
+	redirect_referrer if logged_in?
+	push request.referrer unless inside_stack?
+	return unless request.post?
 
-    if user_login
-      flash[:good] = "Welcome back #{user.login}"
-      redirect rs(:after_login)
+	# default Helper::User method argument is 'creds = request.params'
+	if user_login   
+	    flash[:good] = "Welcome back #{user.login}"
+	    redirect rs(:after_login)
+	end
     end
-  end
 
-  # This method is simply to check whether we really did login and the 
-  # browser sends us a cookie, if we're not logged in by now it would
-  # indicate that the client doesn't support cookies or has it disabled
-  # and so unable to use this site.
-  # For some reason, the arora seems to have problems handling cookies on
-  # localhost from rack.
-  def after_login
-    if logged_in?
-#      answer ProfileController.r(user.login)
-	answer ListController.r
-    else
-      redirect rs(:login, :fail => :session)
+    def after_login
+	if logged_in?
+	    answer MainController.r	# default to home if nothing on stack
+	else
+	    redirect rs(:login, :fail => :session)
+	end
     end
-  end
 
-  def logout
-    user_logout
-    flash[:good] = "You logged out successfully"
-    redirect MainController.r(:index)
-  end
+    def logout
+	user_logout
+	flash[:good] = "You logged out successfully"
+	redirect MainController.r(:index)
+    end
 
- end
+end
