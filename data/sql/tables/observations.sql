224d84b95dd4944be43cdde8adceb6f19e8e1836
Nigel Garside <nigel.garside@gmail.com>
2010-07-20 13:52:32 +0100

    SQL to create observations table, based on birdtrack table sighting.

---
+++ data/sql/tables/observations.sql
@@ -0,0 +1,22 @@
+--  SQLite
+
+DROP TABLE IF EXISTS observations;
+
+CREATE TABLE observations
+(
+    user_id CHAR(2)  NOT NULL REFERENCES users
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    bto_code CHAR(2) NOT NULL REFERENCES birds
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    first_date DATE NOT NULL,
+    note TEXT,
+    PRIMARY KEY (user_id, bto_code)
+);
+
+DROP INDEX IF EXISTS idx_observations_user_id;
+CREATE INDEX idx_observations_user_id ON observations (user_id);
+
+DROP INDEX IF EXISTS idx_observations_bto_code;
+CREATE INDEX idx_observations_bto_code ON observations (bto_code);
