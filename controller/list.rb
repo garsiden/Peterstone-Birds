398cbcef7844e59935d4c163219681cfcef124e8
Nigel Garside <nigel.garside@gmail.com>
2010-07-26 18:32:42 +0100

    Remove unused edit/delete methods from list controller
    ListCOntroller now only returns data and does not modify, which is now
    handled by the new MyList controller. The earliest observation is
    returned based on the first_date field by adding to an array and
    sorting.

--- controller/list.rb
+++ controller/list.rb
@@ -1,75 +1,22 @@
 class ListController < Controller
-    helper :aspect
 
     def initialize
         @isodd = false
     end
 
-    def index(login = nil)
-        @user = login_or_user(login)    # base class method
+    def index
         @title = "Peterstone List"
-        @headings = ['Code', 'Species Name', 'First Date', '' ]
-        if login
-            #@sightings = @user.observations.sort! {|a,b| b.first_date <=> a.first_date}
-            @sightings = @user.observations_dataset.eager(:bird).order(:first_date.desc)
-        else
-            @sightings = Observation.eager(:bird).order(:first_date.desc)
-        end
-    end
+        @headings = ['Code', 'Species Name', 'First Date', 'Observer' ]
+        obs = Observation.eager(:bird).order(:bto_code, :first_date)
 
-    def edit
-        bto_code = request[:bto_code]
-        @user = user
-        @title = "Edit Entry"
-        @legend = "Edit Entry"
-        @submit = "Update Entry"
-        @ob = Observation[user.user_id, bto_code]
-        save                            # private method
-    end
-
-    def new 
-        @user = user                    # Helper::User instance method
-
-        if not request.post?
-            @legend = "New Entry"
-            @title = @legend
-            @submit = "Create Entry"
-            @birds = Bird.filter(:bto_code=> Observation.filter(:user_id=>user.user_id).
-                                 select(:bto_code)).invert.order(:name)
-        end
-        save
-    end
-
-    def delete 
-        if bto_code = request[:bto_code]
-            Observation[user.user_id, bto_code].delete
-        end
-        redirect self.route_self(:/, user.login)
+        @sightings = Array.new
+        bto = nil
+        obs.each {|o| if o.bto_code != bto then @sightings << o; bto = o.bto_code end}
+        @sightings.sort! {|a,b| b.first_date <=> a.first_date } 
     end
 
     def cycle
         (@isodd = !@isodd) ? 'even' : 'odd'
-    end
-
-    # aspect helper method
-    before(:index, :edit, :new, :delete) {login_first}
-
-    private
-
-    def save
-        return unless request.post?
-
-        @ob = Observation.new(:user_id => user.user_id)
-        @ob.first_date = request[:first_date]
-        @ob.note = h(request[:note])
-        @ob.bto_code = request[:bto_code]
-
-        if @ob.save_changes 
-            flash[:good] = "Created observation"
-            redirect self.route_self(:/, @user.login)
-        else
-            flash[:bad] = "Couldn't create observation"
-        end
     end
 
 end
