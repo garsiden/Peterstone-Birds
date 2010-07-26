d7101db2b11955fae19759550825460391e1da01
Nigel Garside <nigel.garside@gmail.com>
2010-07-26 18:59:54 +0100

    Get first obs from model, not list controller
    New class methos added to Observation that returns a sorted array of
    first observations, as per code previously in ListController.

--- controller/list.rb
+++ controller/list.rb
@@ -7,12 +7,7 @@ class ListController < Controller
     def index
         @title = "Peterstone List"
         @headings = ['Code', 'Species Name', 'First Date', 'Observer' ]
-        obs = Observation.eager(:bird).order(:bto_code, :first_date)
-
-        @sightings = Array.new
-        bto = nil
-        obs.each {|o| if o.bto_code != bto then @sightings << o; bto = o.bto_code end}
-        @sightings.sort! {|a,b| b.first_date <=> a.first_date } 
+        @sightings = Observation.first_observations
     end
 
     def cycle
--- model/observation.rb
+++ model/observation.rb
@@ -1,12 +1,12 @@
 class Observation < Sequel::Model
 
     set_schema do
-	foreign_key :bto_code, :birds,
-	    :type=>String, :size=>2, :null=>false
-	foreign_key :user_id, :users, :type=>String, :size=>2, :null=>false
-	Date :first_date
-	String :note
-	primary_key [:user_id, :bto_code]
+        foreign_key :bto_code, :birds,
+            :type=>String, :size=>2, :null=>false
+        foreign_key :user_id, :users, :type=>String, :size=>2, :null=>false
+        Date :first_date
+        String :note
+        primary_key [:user_id, :bto_code]
     end
 
     create_table unless table_exists?
@@ -14,8 +14,25 @@ class Observation < Sequel::Model
     many_to_one :bird, :key=>'bto_code'
     many_to_one :user
 
+
+    def self.first_observations
+
+        obs = self.eager(:bird).order(:bto_code, :first_date)
+
+        first_obs = Array.new
+        bto = nil
+        obs.each do |o|
+            if o.bto_code != bto
+                first_obs << o
+                bto = o.bto_code
+            end
+        end
+        first_obs.sort! {|a,b| b.first_date <=> a.first_date }
+    end
+
     if empty?
-	create :user_id=>'NG', :bto_code=>'PE', :first_date=>'2004-01-01'
-	create :user_id=>'EW', :bto_code=>'DR', :first_date=>'2004-01-01'
+        create :user_id=>'NG', :bto_code=>'PE', :first_date=>'2004-01-01'
+        create :user_id=>'EW', :bto_code=>'DR', :first_date=>'2004-01-01'
     end
 end
+
