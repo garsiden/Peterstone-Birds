7e5d380da8a6d5f74e269d14c09e8d27d991c4c2
Nigel Garside <nigel.garside@gmail.com>
2010-07-29 18:50:51 +0100

    Add delete command and bird name to SQL.

--- data/sql/updates/ew_list_cleanup.sql
+++ data/sql/updates/ew_list_cleanup.sql
@@ -1,4 +1,8 @@
 CREATE OR REPLACE VIEW ew_list_cleanup AS
 
-SELECT * FROM observations
-       WHERE bto_code IN(SELECT bto_code FROM summary_by_user WHERE NG=EW) AND user_id ='EW';
+ SELECT o.bto_code, b.name FROM observations o, birds b
+       WHERE o.bto_code IN(SELECT bto_code FROM summary_by_user WHERE NG=EW) AND user_id ='EW'
+       AND o.bto_code = b.bto_code;
+
+--DELETE  FROM observations
+--       WHERE bto_code  IN(SELECT bto_code FROM summary_by_user WHERE NG=EW) AND user_id ='EW';
