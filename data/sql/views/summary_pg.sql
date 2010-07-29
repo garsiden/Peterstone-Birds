f594865f2d1b46117f56d31bf08f5e94ed747a8c
Nigel Garside <nigel.garside@gmail.com>
2010-07-29 17:46:33 +0100

    Change view for new table names.

--- data/sql/views/summary_pg.sql
+++ data/sql/views/summary_pg.sql
@@ -1,9 +1,9 @@
-DROP VIEW IF EXISTS summary_pg;
+DROP VIEW IF EXISTS summary_by_user;
 
-CREATE VIEW summary_pg AS
+CREATE VIEW summary_by_user AS
 SELECT b.bto_code, b.name,
 MIN(CASE WHEN user_id = 'NG' THEN first_date ELSE NULL END) as NG,
 MIN(CASE WHEN user_id = 'EW' THEN first_date ELSE NULL END) AS EW
-FROM species b, sightings g
-WHERE b.bto_code = g.bto_code
+FROM birds b, observations o
+WHERE b.bto_code = o.bto_code
 GROUP BY b.bto_code, b.name;
