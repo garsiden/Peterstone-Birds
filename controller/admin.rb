df635aab1f3bdb67e8301f70f01695df9828c0e5
Nigel Garside <nigel.garside@gmail.com>
2010-08-04 20:57:57 +0100

    Add Admin functions for users
    List, edit and add new users.

--- controller/admin.rb
+++ controller/admin.rb
@@ -1,7 +1,51 @@
 class AdminController < Controller
 
-  def index
-    @title = "Welcome to Peterstone!"
-  end
+    helper :aspect
 
+    before(:users, :user_edit, :user_new) {login_first}
+
+    def initialize
+        @isodd = false
+    end
+
+    def cycle
+        (@isodd = !@isodd) ? 'even' : 'odd'
+    end
+
+    def users
+        @title = "Users"
+        @headings = %w( ID Login Name Created Updated ) << ' '
+        @users = Sequel::Model::User.order(:created_at)
+    end
+
+    def edit_user 
+        user_id = request[:user_id]
+        @user = Sequel::Model::User[user_id]
+
+        if not request.post?
+            @title = ' Edit User'
+            @submit = "Update"
+            @legend = "Edit User #{@user.name}"
+        else
+            @user.login = request[:login]
+            @user.name = request[:name]
+            if @user.save_changes
+                redirect self.route_self(:users)
+            end
+        end
+    end
+
+    def new_user
+
+        if not request.post?
+            @title = "Add User"
+            @submit = "Submit"
+            @legend = @title
+        else
+            user = Sequel::Model::User.prepare(request)
+            if user.save
+                redirect self.route_self(:users)
+            end
+        end
+    end
 end
