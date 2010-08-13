b204811213d6b1d51d2d970d8b235c57d8dc59f0
Nigel Garside <nigel.garside@gmail.com>
2010-08-13 13:50:51 +0100

    Simple scripts to maintain CSS files from SASS

---
+++ scripts/sass_update.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+#
+# run from within main project directory (peterstonebirds/)
+
+exec sass --style expanded --update public/css:/public/css/sass
---
+++ scripts/sass_watch.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+#
+# run from within main project directory (peterstonebirds/)
+
+exec sass --style expanded --watch public/css/sass:public/css
