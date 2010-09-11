cb0d038c5c965afa13992bfd89435ad221452f14
Nigel Garside <nigel.garside@gmail.com>
2010-09-11 16:23:02 +0100

    Revert to using Ruby 1.8, so require rubygems

--- start.rb
+++ start.rb
@@ -3,5 +3,7 @@
 # All application related things should go into `app.rb`, this file is simply
 # for options related to running the application locally.
 
+require 'rubygems'
+
 require File.expand_path('app', File.dirname(__FILE__))
 Ramaze.start(:adapter => :mongrel, :port => 7000, :file => __FILE__)
