6db553893fff7d7c04008f0a6a32c9cc9384ccef
Nigel Garside <nigel.garside@gmail.com>
2010-09-18 15:03:28 +0100

    Return to add/edit page, with post data, after input failure

--- controller/mylist.rb
+++ controller/mylist.rb
@@ -4,7 +4,7 @@ class MyListController < Controller
     helper :aspect
 
     def initialize
-        @bto_code = request[:bto_code]
+        @bto_code = request[:bto_code] || 'XX'
         super
     end
 
@@ -16,7 +16,11 @@ class MyListController < Controller
     end
 
     def edit
-        @ob = Observation[user.user_id, @bto_code]
+        if not request.post?
+            session.delete(:ob) if (@ob = session[:ob])
+        end            
+
+        @ob ||= Observation[user.user_id, @bto_code]
         @title += " - Edit Entry for #{@ob.bird.name}"
         @legend = "Edit Entry"
         @submit = "Update Entry"
@@ -25,16 +29,20 @@ class MyListController < Controller
 
     def new 
         if not request.post?
+            # set up page
             @title += " - New Entry"
             @legend = "New Entry"
             @submit = "Create Entry"
             @birds = user.unseen_list.all
-            @birds.insert(0, Bird.new({:bto_code=>"XX", :name=>"-- Select Species -- "}))
-        else
-            @ob = Observation.new(:user_id => user.user_id,
-                                  :bto_code => @bto_code)
+            @birds.insert(0, Bird.new({:bto_code=>"XX",
+                                      :name=>"-- Select Species -- "}))
+            # redirected after error?
+            session.delete(:ob) if (@ob = session[:ob])
         end
 
+        # ob required in all circumstances
+        @ob ||= Observation.new(:user_id => user.user_id,
+                                :bto_code => @bto_code)
         save
     end
 
@@ -50,16 +58,34 @@ class MyListController < Controller
 
     def save
         return unless request.post?
-
-        first_date = Date.strptime(request[:first_date], "%d-%m-%Y")
-        @ob.first_date = first_date
         @ob.note = h(request[:note])
+        first_date = validate_date(request[:first_date])
 
-        if @ob.save_changes 
-            flash[:good] = "Created observation"
+        unless first_date
+            @ob.first_date = request[:first_date]
+            flash[:error] = 'Unable to add sighting'
+            session[:ob] = @ob
+            puts request.to_s
+            puts request.request_uri()
+            redirect request.request_uri()
+        else
+            @ob.first_date = first_date
+        end
+
+        unless @ob.save_changes == false
             redirect self.route_self(:/, user.login)
         else
             flash[:bad] = "Couldn't create observation"
+            redirect_referrer
+        end
+    end
+
+    def validate_date (date_string)
+        begin
+            return Date.strptime(date_string, "%d-%m-%Y")
+        rescue => ex
+            flash[:date_parse_error] = "Error: #{ex.message}"
+            return nil
         end
     end
 end
