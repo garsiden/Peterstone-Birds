ad7f7881fc4427bdccbebde40d14902d8ed6826d
Nigel Garside <nigel.garside@gmail.com>
2010-10-12 12:29:16 +0100

    Remove site_id reference from list table

--- data/sql/tables/list.sql
+++ data/sql/tables/list.sql
@@ -1,25 +1,19 @@
---  $Id$
---  SQLite
+--  Table to hold Peterstone lists only
 
 DROP TABLE IF EXISTS list;
 
 CREATE TABLE list
 (
-    list_id		CHAR(8) NOT NULL PRIMARY KEY,
-    site_id		CHAR(2) NOT NULL REFERENCES site(site_id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
+    sub_id		VARCHAR(50) NOT NULL PRIMARY KEY,
     list_date	DATE    NOT NULL,
     start_time	TIME    NOT NULL,
     end_time	TIME    NOT NULL,
 
-    UNIQUE (site_id, list_date),
     CHECK(start_time < end_time),
-    CHECK(LENGTH(list_id) = 8)
+    CONSTRAINT list_date_time_uniq UNIQUE (list_date, start_time)
 );
+
+ALTER TABLE list OWNER TO garsiden;
 
 DROP INDEX IF EXISTS idx_list_date;
 CREATE INDEX idx_list_date ON list (list_date);
-
-DROP INDEX IF EXISTS idx_list_site_id;
-CREATE INDEX idx_list_site_id ON list (site_id);
