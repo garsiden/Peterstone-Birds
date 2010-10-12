c8b7aef403f394677633a599b08f47331570115f
Nigel Garside <nigel.garside@gmail.com>
2010-10-12 18:04:39 +0100

    Simplify method by sharing column headings and keys array

--- controller/report.rb
+++ controller/report.rb
@@ -12,17 +12,11 @@ class ReportController < Controller
         ds  = Report.first_observations
         @first_obs = ds.all
 
-        # display 10 columns (last 7 years)
-        # gate array of table headings
-        ncols = @first_obs[0].keys.count
-        first = ncols - 8
-        last  = ncols - 2 
-        @headings = ['Species']
-        first.upto(last) { |y| @headings << "20%02i" % y }
-        @headings.concat %w[ Earliest Latest ]
-
         # get array of keys for last 7 years
         keys = @first_obs[0].keys.map { |k| k.to_s }
-        @years = keys.grep(/^year\d\d$/).sort.last(7).map { |y| y.to_sym }
+        @headings =  keys.grep(/^\d{4}$/).sort.last(7)
+        @years = @headings.map { |y| y.to_sym }
+        @headings.unshift('Species')
+        @headings.concat %w[ Earliest Latest ]
     end
 end
