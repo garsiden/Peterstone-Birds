72af2ba31affb150e4560981373157b2e059d259
Nigel Garside <nigel.garside@gmail.com>
2010-10-12 16:03:47 +0100

    Use an each block to select first_obs years to display
    Select the latest 7 years to display from all the years columns available, and use an each block on an array of symbols to provide the table columns

--- controller/report.rb
+++ controller/report.rb
@@ -9,7 +9,20 @@ class ReportController < Controller
 
     def first_observations
         @title += " - First Observations"
-        @first_obs = Report.first_observations
-        @headings = %w( Species 2004 2005 2006 2007 2008 2009 2010 Earliest Latest )
+        ds  = Report.first_observations
+        @first_obs = ds.all
+
+        # display 10 columns (last 7 years)
+        # gate array of table headings
+        ncols = @first_obs[0].keys.count
+        first = ncols - 8
+        last  = ncols - 2 
+        @headings = ['Species']
+        first.upto(last) { |y| @headings << "20%02i" % y }
+        @headings.concat %w[ Earliest Latest ]
+
+        # get array of keys for last 7 years
+        keys = @first_obs[0].keys.map { |k| k.to_s }
+        @years = keys.grep(/^year\d\d$/).sort.last(7).map { |y| y.to_sym }
     end
 end
