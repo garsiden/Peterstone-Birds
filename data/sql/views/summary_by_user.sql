cf4f8788d10cbd842b5202d90ed33ecbd856019f
Nigel Garside <nigel.garside@gmail.com>
2010-10-12 12:51:18 +0100

    Add AP as new user column

--- data/sql/views/summary_by_user.sql
+++ data/sql/views/summary_by_user.sql
@@ -2,8 +2,9 @@ DROP VIEW IF EXISTS summary_by_user;
 
 CREATE VIEW summary_by_user AS
 SELECT b.bto_code, b.name,
-MIN(CASE WHEN user_id = 'NG' THEN first_date ELSE NULL END) as NG,
-MIN(CASE WHEN user_id = 'EW' THEN first_date ELSE NULL END) AS EW
+MIN(CASE WHEN user_id = 'AP' THEN first_date ELSE NULL END) AS AP,
+MIN(CASE WHEN user_id = 'EW' THEN first_date ELSE NULL END) AS EW,
+MIN(CASE WHEN user_id = 'NG' THEN first_date ELSE NULL END) AS NG
 FROM birds b, observations o
 WHERE b.bto_code = o.bto_code
 GROUP BY b.bto_code, b.name;
