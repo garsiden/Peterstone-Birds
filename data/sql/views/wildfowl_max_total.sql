0e6269a2f548fb20f44674c25071cd6f2ef426a6
Nigel Garside <nigel.garside@gmail.com>
2010-10-15 15:33:43 +0100

    Replace list_view with sub-query

--- data/sql/views/wildfowl_max_total.sql
+++ data/sql/views/wildfowl_max_total.sql
@@ -14,11 +14,15 @@
         max(CASE WHEN mnth =  4 THEN max_count ELSE NULL END) AS apr,
         max(max_count) as max
    FROM (
-      SELECT bto_code, name as species, max(lv.species_count) AS max_count,
+      SELECT bto_code, species_name as species, max(lv.species_count) AS max_count,
       date_part('month', lv.list_date) AS mnth,
       'Total' AS winter
-      FROM list_view lv
-      WHERE lv.list_date >= '2004-07-01'
+      FROM (
+      SELECT l.sub_id, l.list_date, g.bto_code, b.name AS species_name, g.species_count,
+        FROM sightings g, birds b, lists l
+        WHERE g.bto_code = b.bto_code
+        AND g.sub_id = l.sub_id) lv
+       WHERE lv.list_date >= '2004-07-01'
       GROUP BY bto_code, species, mnth, winter
       HAVING bto_code in ('SU', 'PT', 'SV', 'WN', 'T', 'OC', 'BW', 'RK', 'GV', 'KN', 'DN', 'RP', 'BA', 'L') AND
       date_part('month', lv.list_date) IN( 7,8,9,10,11,12,1,2,3,4)) x
