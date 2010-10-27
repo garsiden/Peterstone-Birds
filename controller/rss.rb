d1b11f3f96cc14cf5d4037378d2e4bb99bb66423
Nigel Garside <nigel.garside@gmail.com>
2010-10-27 19:34:44 +0100

    Change RSS feed to 7 lists and use loop to create output

--- controller/rss.rb
+++ controller/rss.rb
@@ -4,25 +4,22 @@ class RssController < Controller
     engine :None
 
     def index
-        lists = List.latest
-        url = "http://#{request.http_host}" #localhost:7000'
+        lists = List.latest 7
+        url = "http://#{request.http_host}"
         version = "2.0" # ["0.9", "1.0", "2.0"]
 
         content = RSS::Maker.make(version) do |m|
-            m.channel.title = "Example BirdTrack RSS feed"
+            m.channel.title = "Peterstone Birds Daily Lists"
             m.channel.link = url
-            m.channel.description = "Latest BirdTrack lists as RSS feed"
-            m.items.do_sort = false # sort items by date
+            m.channel.description = "Latest Peterstone Birds lists as RSS feed"
+            m.items.do_sort = true # sort items by date
 
-            i = m.items.new_item
-            i.title = "List for #{lists[0][:list_date].strftime('%d %B %Y')}"
-            i.link = "#{url}/list/daily?sub_id=#{lists[0][:sub_id]}"
-            i.date = Time.parse(lists[0][:list_date].to_s)
-
-            i = m.items.new_item
-            i.title = "List for #{lists[1][:list_date].strftime('%d %B %Y')}"
-            i.link = "#{url}/daily?sub_id=#{lists[1][:sub_id]}"
-            i.date = Time.now #lists[1][:list_date] #Time.now
+            lists.each do |list|
+                item = m.items.new_item
+                item.title = "List for #{list[:list_date].strftime('%A %e %B')}"
+                item.link = "#{url}/list/daily?sub_id=#{list[:sub_id]}"
+                item.date = list.date_and_time
+            end
         end
 
         content
