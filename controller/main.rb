cfff44c231ecd0039dc16e7a6609e571ab7a7a54
Nigel Garside <nigel.garside@gmail.com>
2010-10-29 15:12:54 +0100

    Add a What's New page, data sourced from a yaml file

--- controller/main.rb
+++ controller/main.rb
@@ -1,12 +1,6 @@
-# Default url mappings are:
-#  a controller called Main is mapped on the root of the site: /
-#  a controller called Something is mapped on: /something
-# If you want to override this, add a line like this inside the class
-#  map '/otherurl'
-# this will force the controller to be mounted on: /otherurl
+require 'yaml'
 
 class MainController < Controller
-    # the index action is called automatically when no other action is specified
 
     def index
         @bird_latest = Observation.latest
@@ -21,10 +15,10 @@ class MainController < Controller
         @title = "About " + @title
     end
 
-    # the string returned at the end of the function is used as the html body
-    # if there is no template for the action. if there is a template, the string
-    # is silently ignored
-    def notemplate
-        "there is no 'notemplate.xhtml' associated with this action"
+    def whats_new
+        @title += " - What's New"
+        @header = "What's New"
+        @items = YAML::load_file('yaml/whats_new.yaml')
+        @items.sort! {|a,b| b['term'] <=> a['term'] }
     end
 end
