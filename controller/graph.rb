0687fd18319acea08e73116a898b49a93e977c92
Nigel Garside <nigel.garside@gmail.com>
2010-11-02 14:41:34 +0000

    Add basic breadcrumb method fro graph pages

--- controller/graph.rb
+++ controller/graph.rb
@@ -4,16 +4,27 @@ class GraphController < Controller
     layout 'nomenu_layout'
     
     def index
-        @title += "- Graphs"
+        @title += " - Graphs"
         @caption = 'Graphs'
         @graphs = YAML::load_file('yaml/graphs.yaml')
     end
 
     def show graph
-
-        @title += "#{graph} Graph"
+        text = graph.gsub(/\/|_/, ' ').gsub(/\b./) { |m| m.upcase }
+        @title += " - #{text} Graph"
         @graph = graph
-        p graph
+    end
 
+    private
+    
+    def breadcrumb path
+        path =~ /(\w+$)/
+        mapping = path.sub(/^\/graph\//, '')
+        text = mapping.gsub(/\/|_/, ' ').gsub(/\b./) { |m| m.upcase }
+        bread = []
+        bread << anchor('Home', MainController.r('/'))
+        bread << anchor('Graphs', '/')
+        bread << anchor(text, mapping)
+        bread.join(' >> ')
     end
 end
