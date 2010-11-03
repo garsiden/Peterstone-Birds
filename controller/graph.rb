0ca024403f379776e4c2679a5fada3f1e7e6c5e3
Nigel Garside <nigel.garside@gmail.com>
2010-11-03 19:30:30 +0000

    Display text description on graph page

--- controller/graph.rb
+++ controller/graph.rb
@@ -2,24 +2,30 @@ class GraphController < Controller
     require 'yaml'
 
     layout 'nomenu_layout'
-    
+
+    def initialize
+        super
+        @graphs = YAML::load_file('yaml/graphs.yaml')
+    end
+
     def index
         @title += " - Graphs"
         @caption = 'Graphs'
-        @graphs = YAML::load_file('yaml/graphs.yaml')
     end
 
     def show graph
         text = graph.gsub(/\/|_/, ' ').gsub(/\b./) { |m| m.upcase }
         @title += " - #{text} Graph"
         @graph = graph
+        @text =  @graphs[graph]['text']
+        @text.gsub!("\n", '<br/>') if @text
     end
 
     private
-    
+
     def breadcrumb path
         path =~ /(\w+$)/
-        mapping = path.sub(/^\/graph\//, '')
+            mapping = path.sub(/^\/graph\//, '')
         text = mapping.gsub(/\/|_/, ' ').gsub(/\b./) { |m| m.upcase }
         bread = []
         bread << anchor('Home', MainController.r('/'))
