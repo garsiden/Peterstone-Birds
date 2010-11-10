f9aa4e94645a7f087f9b872f7a221683cf25f61a
Nigel Garside <nigel.garside@gmail.com>
2010-11-10 15:02:30 +0000

    Try only setting Gruff font property if env variable set

--- lib/pbgraph.rb
+++ lib/pbgraph.rb
@@ -41,9 +41,7 @@ class Gruff::Base
         @title_font_size = 24.0
         @legend_font_size = 20.0
         @marker_font_size = 20.0
-        @font= ENV['MAGICK_FONT_PATH'] ?
-            ENV['MAGICK_FONT_PATH'] + '/Vera.ttf' :
-            nil 
+        @font= ENV['MAGICK_FONT_PATH'] + '/Vera.ttf' if ENV['MAGICK_FONT_PATH']
     end
 
     def set_data
